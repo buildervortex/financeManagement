616573c8843d2243f2a708bd0fee028906b4dbf0
galahirudilhara <galahirudilhara@gmail.com>
2024-08-31 11:00:37 +0530

    refactor the frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,20 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import AccountRegister from './pages/AccountRegister';
+import AccountRegisterPage from './pages/AccountRegister';
+
 
 function App() {
-  const navItems = [
-    { name: "Customers", to: "/" },
-    { name: "Create", to: "/customers" }
-  ]
   return (
     <Router>
       <div className="App mx-auto w-5/6">
-        {/* <NavBar navItems={navItems}></NavBar> */}
         <Routes>
-          <Route path="/" element={<AccountRegister></AccountRegister>}></Route>
-          {/* <Route path='/customers' element={<CustomerForm></CustomerForm>}></Route> */}
+          <Route path="/" element={<AccountRegisterPage></AccountRegisterPage>}></Route>
         </Routes>
       </div>
     </Router>
   );
 }
+
+
 
 export default App;
